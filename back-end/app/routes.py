36958db49b9587b7ac7a07eca9af9d55c3eb9a87
Leadhr <mhgamble1@gmail.com>
2020-07-10 10:56:15 -0500

    login route nearly complete

--- back-end/app/routes.py
+++ back-end/app/routes.py
@@ -1,6 +1,20 @@
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, request
+from flask_cors import CORS
+from flask_login import current_user, login_user
+from app.models import User
 from app import app
+
+# CORS
+CORS(app, resources=r'/api/*')
+
 
 @app.route('/api/login', methods=['GET', 'POST'])
 def login():
-    return 'hello world'
+    # print("request: ", request.json)
+    response = request.json
+    user = User.query.filter_by(username=response["email"]).first()
+    if user is None or not user.check_password(response["password"]):
+        print('no user with that name')
+        return 'no user with that name'
+    login_user(user)
+    return "success, logged in"
